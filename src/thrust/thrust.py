e5aac0e9f9bff754dfb2571fe3045dbac3ea5693
cTurtle98 <ciaran@cturtle98.com>
2022-10-21 10:26:03 -0700

    add thruster naming convention

--- src/thrust/thrust.py
+++ src/thrust/thrust.py
@@ -3,6 +3,26 @@
 cabrillo_rov_2023
 thrust module
 
-this module does the math from twist ros messages to pwm outputs on the thrust box pwm board
+this module takes the twist messages and translates them to servo angles for thrust box pwm.
 '''
 
+# the ros client library for python
+import rclpy
+
+# importing the idea of node so I can define a subscriber
+from rclpy.node import Node
+
+# twish messages are what we are taking as input
+from geometry_msgs.msg import Twist
+
+
+### thruster naming
+#
+# FRU = Front Right Up
+# FLU = Front Left Up
+# FRD = Front Right Down
+# FLD = Front Left Down
+# RRU = Rear Right Up
+# RLU = Rear Left Up
+# RRD = Rear Rght Down
+# RLD = Rear Left Down
