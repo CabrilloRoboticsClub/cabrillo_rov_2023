0735a89d46473fe4eeef650d0e2acaec8605b752
Emma Barger <LIGHTHOUSE@Katies-MacBook-Air.local>
2022-10-31 17:51:27 -0700

    new file for this sensor

---
+++ src/sensors/temp_humid.py
@@ -0,0 +1,37 @@
+'''
+temp_humid.py
+
+this ros2 node component grabs temp and humid sensor data and publishes it 
+
+Copyright (C) 2022-2023 Cabrillo Robotics Club
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Cabrillo Robotics Club
+6500 Soquel Drive Aptos, CA 95003
+cabrillorobotics@gmail.com
+'''
+
+# dependancies for ROS
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+# dependancies for BME280
+import board
+from adafruit_bme280 import basic as adafruit_bme280
+
+class temp_humid(Node):
+    
