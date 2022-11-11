61a0e180cabbcaed257f192191a24a2cad34073c
Robert Garrett <robertgarrett404@gmai.com>
2022-11-11 13:04:53 -0600

    Created subscriber node

---
+++ src/py_pubsub/py_pubsub/subscriber.py
@@ -0,0 +1,29 @@
+
+import rcylpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Sub(Node):
+
+    def __init__(self):
+        super().__init__("Subby")
+        self.subscription = self.create_subscription (
+            String,
+            "sample_topic",
+            self.receive,
+            10
+        )
+
+    def receive(self, msg):
+        self.get_logger().info("I heard a message! : '%s'" % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    minSub = Sub()
+    rclpy.spin(minSub)
+
+    minSub.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
