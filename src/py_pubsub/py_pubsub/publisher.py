e8ee28acc3d578e571097c956df70a4cb53fe0f6
Robert Garrett <robertgarrett404@gmai.com>
2022-11-11 12:00:26 -0600

    Simple publisher
    Just the ROS2 example publisher with some small naming changes.

---
+++ src/py_pubsub/py_pubsub/publisher.py
@@ -0,0 +1,30 @@
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Pub(Node):
+
+    def __init__(self):
+        super().__init__("Dummy Publisher")
+        self.publisher = self.create_publisher(String, "sample topic", 10)
+        self.timer = self.create_timer(1, self.emit_message)
+        self.i = 0
+
+    def emit_message(self):
+        mesg = String() # gotta use a ROS String, not Pythong str()
+        mesg.data = "Hey, what's up scrub. Check out this counter %d" % self.i
+        self.publisher.publish(mesg)
+        self.get_logger().info("publishing: '%s'" % mesg.data)
+        self.i += 1
+
+def main(args = None):
+    rclpy.init(args=args)
+    minPub = Pub()
+    rclpy.spin(minPub)
+
+    minPub.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
