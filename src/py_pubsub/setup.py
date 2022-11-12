9417ec8713b87c750bf1d139461e1615111e1240
Robert Garrett <robertgarrett404@gmai.com>
2022-11-12 09:16:20 -0600

    Fix listener package path in setup.py
    I've gotta get a better handle on my typos and missplellings

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'talker = py_pubsub.publisher:main',
-            'listener = py_pysub.subscriber:main',
+            'listener = py_pubsub.subscriber:main',
         ],
     },
 )
