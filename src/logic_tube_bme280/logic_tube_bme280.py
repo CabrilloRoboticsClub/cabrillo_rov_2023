45353396f193486d7dd2952654681fc636743633
cTurtle98 <ciaran@cturtle98.com>
2022-11-29 09:21:49 +0000

    tested both of these work on the pi

--- src/logic_tube_bme280/logic_tube_bme280.py
+++ src/logic_tube_bme280/logic_tube_bme280.py
@@ -9,8 +9,9 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 
 # 0x77 is address for bme280 with un-bridged address pads
 
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x77)
+logic_tube_bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x77)
 
-print("\nTemperature: %0.1f C" % bme280.temperature)
-print("Humidity: %0.1f %%" % bme280.humidity)
-print("Pressure: %0.1f hPa" % bme280.pressure)
+print("logic tube")
+print("\nTemperature: %0.1f C" % logic_tube_bme280.temperature)
+print("Humidity: %0.1f %%" % logic_tube_bme280.humidity)
+print("Pressure: %0.1f hPa" % logic_tube_bme280.pressure)
--- src/thrust_box_bme280/thrust_box_bme280.py
+++ src/thrust_box_bme280/thrust_box_bme280.py
@@ -9,8 +9,9 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 
 # 0x76 is address for bme280 with bridged address pads
 
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
+thrust_box_bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
 
-print("\nTemperature: %0.1f C" % bme280.temperature)
-print("Humidity: %0.1f %%" % bme280.humidity)
-print("Pressure: %0.1f hPa" % bme280.pressure)
+print("thrust box")
+print("\nTemperature: %0.1f C" % thrust_box_bme280.temperature)
+print("Humidity: %0.1f %%" % thrust_box_bme280.humidity)
+print("Pressure: %0.1f hPa" % thrust_box_bme280.pressure)
