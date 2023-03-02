832bc792a4d54dd662fdb58a1b57964294601cde
OrionOth <orionoth@protonmail.ch>
2023-03-02 12:12:23 -0800

    Use dictionary to map controller

--- src/seahawk_deck/seahawk_deck/motion_controller.py
+++ src/seahawk_deck/seahawk_deck/motion_controller.py
@@ -30,49 +30,56 @@ class MotionController(Node):
         self.get_logger().info(f"Joystick axes: {joy_msg.axes} buttons: {joy_msg.buttons}")
 
         # Compute desired motion in <x, y, z, r, p, y>
-        # Message index 3 and 4 are the right stick left/right and forward/back
-        # Yaw is at position 2 and 5. Domain is [-1, 1]
 
-        """
-        twist_msg = Twist()
-        twist_msg.linear.x  = joy_msg.axes[4] # X 
-        twist_msg.linear.y  = -joy_msg.axes[3]# Y Direction was inverted. Negative added so negative is to the left and positive is to the right
-        twist_msg.linear.z  = joy_msg.axes[1] # Z 
-        twist_msg.angular.x = 0.0 # R 
-        twist_msg.angular.y = 0.0 # P 
-        twist_msg.angular.z = (joy_msg.axes[2] - joy_msg.axes[5]) / 2 # Y 
-        """
-
-        joy = {
-            'left_stick': {'x':scale*-joy_msg.axes[0],'y':joy_msg.axes[1]},
-            'right_stick':{}
+        # CONTROLLER KEYMAPPINGS
+        controller = {
+            'axes': ['left_stick', 'right_stick'],
+            'left_stick': {
+                'x':        -joy_msg.axes[0],
+                'y':        joy_msg.axes[1],
+                'press':    joy_msg.buttons[9],
+            },
+            'right_stick': {
+                'x':        -joy_msg.axes[3],
+                'y':        joy_msg.axes[4],
+                'press':    joy_msg.buttons[10],
+            },
+            'left_trigger': joy_msg.axes[2],
+            'right_trigger':joy_msg.axes[5],
+            'dpad': {
+                'up':       max(joy_msg.axes[7], 0), # +
+                'down':     min(joy_msg.axes[7], 0), # -
+                'right':    max(joy_msg.axes[6], 0), # +
+                'left':     min(joy_msg.axes[6], 0), # -
+            },
+            'a':            joy_msg.buttons[0],
+            'b':            joy_msg.buttons[1],
+            'x':            joy_msg.buttons[2],
+            'y':            joy_msg.buttons[3],
+            'left_bumper':  joy_msg.buttons[4],
+            'right_bumper': joy_msg.buttons[5],
+            'window':       joy_msg.buttons[6],
+            'menu':         joy_msg.buttons[7],
+            'xbox':         joy_msg.buttons[8],
         }
 
+        # NORMALIZE CONTROLLER AXES
+        SCALE_ACCURACY = 10 * 2 # must be even
+        for axis in controller['axes']:
+            angle = math.atan(controller[axis]['x'] / (controller[axis]['y'] + 0.0000000000000001)) # don't want to divide by zero!
+            scale = pow(pow(math.cos(angle), SCALE_ACCURACY) + pow(math.sin(angle), SCALE_ACCURACY), 1 / SCALE_ACCURACY) 
+            
+            controller[axis]['x'] *= scale
+            controller[axis]['y'] *= scale
 
-
-
-
-
-        joy_ls_y = joy_msg.axes[1] # left y
-        joy_ls_x = -joy_msg.axes[0] # left x
-        joy_lt = joy_msg.axes[2] # Triggers
-        joy_rt = joy_msg.axes[5]
-        joy_ang_x  = 0.0 # no roll
-        joy_rs_y = joy_msg.axes[4] # right y
-        joy_rs_x = joy_msg.axes[3] # right x
-
-        scale_acc = 10 * 2 # must be even
-        angle = math.atan(-joy_msg.axes[0] / (joy_msg.axes[1] + 0.0000000000000001))
-        scale = (math.cos(angle)**scale_acc + math.sin(angle)**scale_acc)**(1/scale_acc)
-
-        # INPUTS
+        # BINDINGS
         twist_msg = Twist()
-        twist_msg.linear.x  = scale * joy_ls_y # X (forward)
-        twist_msg.linear.y  = scale * joy_ls_x # Y (right)
-        twist_msg.linear.z  = (joy_lt - joy_rt) / 2 # Z
-        twist_msg.angular.x = joy_ang_x # R 
-        twist_msg.angular.y = joy_rs_y # P 
-        twist_msg.angular.z = joy_rs_x # Y 
+        twist_msg.linear.x  = controller['left_stick']['y'] # X (forward)
+        twist_msg.linear.y  = controller['left_stick']['x']# Y (right)
+        twist_msg.linear.z  = (controller['left_trigger'] - controller['right_trigger']) / 2 # Z
+        twist_msg.angular.x = 0.0 # R 
+        twist_msg.angular.y = controller['right_stick']['y'] # P 
+        twist_msg.angular.z = controller['right_stick']['x'] # Y 
 
         # Send the twist message for debugging.
         self.twist_pub.publish(twist_msg)
@@ -111,13 +118,13 @@ class MotionController(Node):
 
 
         # No roll, we do not want cartwheels 
-        motor_msg.data[0] = twist_msg.linear.x - twist_msg.linear.y + twist_msg.angular.z
+        motor_msg.data[0] = twist_msg.linear.x - twist_msg.linear.y - twist_msg.angular.z
         # motor_msg.data[1] = twist_msg.linear.z + twist_msg.angular.y
-        motor_msg.data[2] = -twist_msg.linear.x - twist_msg.linear.y - twist_msg.angular.z
+        motor_msg.data[2] = -twist_msg.linear.x - twist_msg.linear.y + twist_msg.angular.z
         # motor_msg.data[3] = twist_msg.linear.z - twist_msg.angular.y
-        motor_msg.data[4] = -twist_msg.linear.x + twist_msg.linear.y + twist_msg.angular.z
+        motor_msg.data[4] = -twist_msg.linear.x + twist_msg.linear.y - twist_msg.angular.z
         # motor_msg.data[5] = twist_msg.linear.z - twist_msg.angular.y
-        motor_msg.data[6] = twist_msg.linear.x + twist_msg.linear.y - twist_msg.angular.z
+        motor_msg.data[6] = twist_msg.linear.x + twist_msg.linear.y + twist_msg.angular.z
         # motor_msg.data[7] = twist_msg.linear.z + twist_msg.angular.y
 
         # Publish data to the motors
