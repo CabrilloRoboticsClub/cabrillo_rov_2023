557989e04ac359381b1f6a073031aca2a0c38e9c
Robert Garrett <robertgarrett404@gmail.com>
2023-03-19 16:39:55 -0500

    Fixup ROV setup.py, and supply ament marker file
    Fill out (some) details of seahawk_rov/setup.py, and re-enable the
    ament marker resource install. This installs a file named the same as
    the package. It's source-tree location is ${package}/resource/, and
    appears to need exactly nothing written in it.
    
    (Also: Do the same for the "our_examples" package. It's not real
    important, but it'll cause `colcon` to abort builds if it doesn't also
    work).

--- src/seahawk_rov/setup.py
+++ src/seahawk_rov/setup.py
@@ -8,17 +8,16 @@ setup(
     name=package_name,
     version='0.0.1',
     # Packages to export
-    packages=[
-        package_name,
-    ],
+    packages=[package_name],
     # Files we want to install, specifically launch files
     data_files=[
         # Install marker file in the package index
-        # ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         # Include our package.xml file
         (os.path.join('share', package_name), ['package.xml']),
+        # The `seahawk_rov` package doesn't actually contain any launch files. They're up a level in the main workspace.
         # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+#        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
     ],
     # This is important as well
     install_requires=['setuptools'],
@@ -34,8 +33,8 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description='My awesome package.',
-    license='TODO',
+    description='Seahawk ROV submersible software',
+    license='AGPLv3',
     # Like the CMakeLists add_executable macro, you can add your python
     # scripts here.
     entry_points={
