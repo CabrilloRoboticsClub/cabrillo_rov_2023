a756369497a94c326b306f16e6eb9e9c9092ebdc
Robert Garrett <robertgarrett404@gmail.com>
2023-03-19 15:13:41 -0500

    Split out the example code into it's own package
    I never meant for the examples to live for very long, but here we are.
    They can at least be made into their own package. They run anywhere with
    a ROS2 install, not only on the ROV.

---
+++ src/our_examples/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = "our_examples"
+
+setup(
+    name=package_name,
+    version="1.0.0",
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name] ),
+        ("share/" + package_name, ["package.xml"] )
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Robert Garrett",
+    maintainer_email="robertgarrett404@gmail.com",
+    description="Example ROS Package providing a publisher and subscriber node.",
+    license="AGPLv3",
+    entry_points={
+        "console_scripts": [
+            "example_pub = seahawk_rov.example_pub:main",
+            "example_sub = seahawk_rov.example_sub:main",
+        ]
+    }
+)
