75f8681d20371d06e5bb02ee2b47645ceb012708
cTurtle98 <ciaran@cturtle98.com>
2023-03-19 17:02:38 -0700

    start the code for the claws

---
+++ src/seahawk_rov/seahawk_rov/logic_tube_motor.py
@@ -0,0 +1,24 @@
+'''
+logic_tube_motor.py
+
+code for controlling the motor hat in the logic tube
+
+Copyright (C) 2022-2023 Cabrillo Robotics Club
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Cabrillo Robotics Club
+6500 Soquel Drive Aptos, CA 95003
+cabrillorobotics@gmail.com
+'''
