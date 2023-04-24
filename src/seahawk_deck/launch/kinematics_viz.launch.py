61b116d787b6ec4e4aa648e0797fc049336b2ca9
OrionOth <orionoth@protonmail.ch>
2023-04-24 14:32:49 -0700

    Update rviz file path

--- src/seahawk_deck/launch/kinematics_viz.launch.py
+++ src/seahawk_deck/launch/kinematics_viz.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', 'rviz/debug_kinematics.rviz']
+            arguments=['-d', os.path.join(get_package_share_path('seahawk_deck'), 'rviz/debug_kinematics.rviz')]
         ),
 
         Node(
