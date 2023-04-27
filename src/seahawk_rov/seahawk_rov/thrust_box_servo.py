579a6fca19ccd67c760541fa0f3c926aef4ecf53
OrionOth <orionoth@protonmail.ch>
2023-04-27 14:24:45 -0700

    1470

--- src/seahawk_rov/seahawk_rov/thrust_box_servo.py
+++ src/seahawk_rov/seahawk_rov/thrust_box_servo.py
@@ -56,5 +56,5 @@ class ThrustBoxServo:
 
     def receive_thruster(self, message:Float32MultiArray):
         for thruster in self.thruster_map:
-            self.kit.servo[thruster].angle = 1505 #self.thruster_offset[thruster] # int(seahawk_rov.float_to_pwm(seahawk_rov.clamp(message.data[thruster], -1.0, 1.0)))
+            self.kit.servo[thruster].angle = 1470 #self.thruster_offset[thruster] # int(seahawk_rov.float_to_pwm(seahawk_rov.clamp(message.data[thruster], -1.0, 1.0)))
             print(f"Thruster {thruster} | {int(seahawk_rov.float_to_pwm(seahawk_rov.clamp(message.data[thruster], -1.0, 1.0)))} | {self.kit.servo[thruster].angle} μs")
