2884bc2e3f8f110355c22cf1768479b07fdbb7e1
OrionOth <orionoth@protonmail.ch>
2023-06-01 15:15:00 -0700

    Relative symlink fix

--- src/seahawk_rov/launch/rov.launch.py
+++ src/seahawk_rov/launch/rov.launch.py
@@ -6,8 +6,8 @@ from launch_ros.actions import Node
 # from math import pi
 
 
-claw_camera_path = os.readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index2')
-top_camera_path = os.readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index2')
+claw_camera_path = '/dev/' + os.path.basename(os.readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index2'))
+top_camera_path = '/dev/' + os.path.basename(os.readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index2'))
 
 v4l2_regex = r"mmal.*\n\s*(/dev/video\d)"
 v4l2_devices = os.popen("v4l2-ctl --list-devices").read()
