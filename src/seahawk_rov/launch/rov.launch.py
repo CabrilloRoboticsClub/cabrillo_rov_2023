6d916d6a576a3ddf9e4d49a24e8247634559fece
OrionOth <orionoth@protonmail.ch>
2023-06-01 15:02:43 -0700

    Use regex of v4l2-ctl command

--- src/seahawk_rov/launch/rov.launch.py
+++ src/seahawk_rov/launch/rov.launch.py
@@ -1,4 +1,4 @@
-from os import readlink
+import os, re
 from sys import exit
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -6,25 +6,13 @@ from launch_ros.actions import Node
 # from math import pi
 
 
-# this is so cursed but it works so
-# basically the reason it works is that the usb cameras always mount 4 /dev/video devices in sequence, and then
-# the csi port camera only has one <10 device it mounts, so depending on the mounting order the csi port camera
-# can be either /dev/video0, /dev/video4, or /dev/video8
-# 0: 3 + 7 = 10
-# 4: 2 + 7 = 9
-# 8: 2 + 6 = 8
-claw_camera_path = readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index2')
-top_camera_path = readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index2')
-match int(claw_camera_path[-1]) + int(top_camera_path[-1]):
-    case 10:
-        front_camera_path = '/dev/video0'
-    case 9:
-        front_camera_path = '/dev/video4'
-    case 8:
-        front_camera_path = '/dev/video8'
-    case _:
-        exit(1)
+claw_camera_path = os.readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index2')
+top_camera_path = os.readlink('/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index2')
 
+v4l2_regex = r"mmal.*\n\s*(/dev/video\d)"
+v4l2_devices = os.popen("v4l2-ctl --list-devices").read()
+
+front_camera_path = re.search(v4l2_regex, v4l2_devices).group(1)
 
 def generate_launch_description():
     return LaunchDescription([
