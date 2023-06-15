95d6f9d1f030da95449d9be95cacb18a7668c5cf
OrionOth <orionoth@protonmail.ch>
2023-06-15 14:02:27 -0700

    Update PWM offset for current servo hat

--- src/seahawk_rov/seahawk_rov/thrust_box_servo.py
+++ src/seahawk_rov/seahawk_rov/thrust_box_servo.py
@@ -42,8 +42,7 @@ class ThrustBoxServo:
 
         # map output channels        
         self.thruster_map = (0,1,2,3,4,5,6,7)
-        #self.pwm_offset = -30 # offset in microseconds
-        self.pwm_offset = 0
+        self.pwm_offset = -100 # offset in microseconds
 
         # instantiate outputs
         self.kit = ServoKit(channels=16, i2c=i2c, address=0x41)
