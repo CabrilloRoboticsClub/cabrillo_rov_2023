7fc8809e25d1e6926515841c235771e3eb1d3b9e
Mike Matera <matera@lifealgorithmic.com>
2023-09-24 20:24:10 -0700

    Make sure front camera is in streaming mode.

--- src/seahawk_rov/launch/rov.launch.py
+++ src/seahawk_rov/launch/rov.launch.py
@@ -3,16 +3,17 @@ import pathlib
 from sys import exit
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
-# from math import pi
-
+import subprocess
 
 claw_camera_path = '/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index2'
 top_camera_path = '/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index2'
+
 v4l2_regex = r"mmal.*\n\s*(/dev/video\d)"
 v4l2_devices = os.popen("v4l2-ctl --list-devices").read()
-
 front_camera_path = re.search(v4l2_regex, v4l2_devices).group(1)
+
+subprocess.run(f'v4l2-ctl --set-ctrl h264_i_frame_period=90 -d {front_camera_path}', shell=True)
+subprocess.run(f'v4l2-ctl --set-ctrl repeat_sequence_header=1 -d {front_camera_path}', shell=True)
 
 def generate_launch_description():
     nodes = [
