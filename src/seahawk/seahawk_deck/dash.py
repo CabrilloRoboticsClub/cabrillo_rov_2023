22a3bffd71f16d3acff0fadacd7841e7e72ae0a5
Steph <steph1111>
2024-01-14 10:14:39 -0800

    Code cleanup - Added python-style dockstrings - Changed TabWidget to take in a paremeter for the tab names and css file path rather than hard coding

--- src/seahawk/seahawk_deck/dash.py
+++ src/seahawk/seahawk_deck/dash.py
@@ -4,10 +4,10 @@ from PyQt5 import QtWidgets as qtw
 import sys
 
 # Size constants
-MAX_WIDTH   = 1862
-MAX_HEIGHT  = 1053
-# MAX_WIDTH   = 500
-# MAX_HEIGHT  = 500
+# MAX_WIDTH   = 1862
+# MAX_HEIGHT  = 1053
+MAX_WIDTH   = 1000  # Temp for debugging
+MAX_HEIGHT  = 600  # Temp for debugging
 
 # Color constants
 COLOR_CONSTS = {
@@ -22,61 +22,75 @@ COLOR_CONSTS = {
 
 
 class MainWindow(qtw.QMainWindow):
+    """
+    Creates a 'MainWindow' which inherits from the 'qtw.QMainWindow' class. 'MainWindow'
+    provides the main application window space to overlay widgets
+    """
     def __init__(self):
+        """
+        Set up the 'MainWindow', overlay 'TabWidget's for multiple dash views, and display window
+        """
         super().__init__()
+
+        # Set up main window
         self.setWindowTitle("SeaHawk II Dashboard")
         self.setStyleSheet(f"background-color: {COLOR_CONSTS['MAIN_WIN_BKG']};")
         self.setGeometry(0, 0, MAX_WIDTH, MAX_HEIGHT)
         
-        self.table_widget = TabWidget(self)
-        self.setCentralWidget(self.table_widget)
+        # Create tabs
+        self.__tab_widget = TabWidget(self, ["Home", "Debug", "Cameras", "Control Mapping"], "dash_styling/tab_widget.txt")
+        self.setCentralWidget(self.__tab_widget)
 
+        # Display window
         self.show()
+        # self.showMaximized() # Uncomment in final
+
 
 class TabWidget(qtw.QWidget):
-    def __init__(self, parent):
+    """
+    Creates a 'TabWidget' which inherits from the 'qtw.QWidget' class. A 'TabWidget' provides 
+    a tab bar and a page area that is used to display pages related to each tab. The tab bar is 
+    shown above the page area. Each tab is associated with a different widget called a page. 
+    Only the current page is shown in the page area; all the other pages are hidden. The user can 
+    show a different page by clicking on its tab
+    """
+
+    def __init__(self, parent: MainWindow, tab_list: list[str], style_sheet_file: str):
+        """
+        Initialize tabs
+        Args:
+            parent: Window where to place tabs
+            tab_list: List of tab names
+            style_sheet_file: Style sheet text file formatted as a CSS f-string
+        """
         super().__init__(parent)
         
         # Define layout of tabs
-        layout = qtw.QVBoxLayout(self)
+        self.__layout = qtw.QVBoxLayout(self)
 
         # Initialize tabs
         self.__tabs = qtw.QTabWidget()
-        self.__tab_dict =  {
-            "Home":             qtw.QWidget(),  # Index 0
-            "Debug":            qtw.QWidget(),  # Index 1
-            "Cameras":          qtw.QWidget(),  # Index 2
-            "User Control Map": qtw.QWidget()   # Index 3
-        }
 
-        # Track index of the current opened tab
-        # self.__prev_tab_index = 0
+        # Create a dict in which the key is the provided name of the tab, and the value is a qtw.QWidget() object
+        self.__tab_dict = {name: qtw.QWidget() for name in tab_list}
 
         # Add tabs
         for name, tab in self.__tab_dict.items():
             self.__tabs.addTab(tab, name)
         
         # Apply css styling
-        with open("dash_styling/tab_widget.txt") as style_sheet:
+        with open(style_sheet_file) as style_sheet:
             self.setStyleSheet(style_sheet.read().format(**COLOR_CONSTS))
         
         # Add tabs to widget
-        layout.addWidget(self.__tabs)
-        self.setLayout(layout)
+        self.__layout.addWidget(self.__tabs)
+        self.setLayout(self.__layout)
 
         # What to do when a tab is clicked
         # self.__tabs.currentChanged.connect(self.__on_click)
 
     # @qtc.pyqtSlot()
     # def __on_click(self):
-
-    
-class HomeDash():
-    pass
-
-
-class DebugDash():
-    pass
 
 
 def main():
