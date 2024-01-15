91d28f08215c50b44f8e7a2e54e289f288ddf7f6
Steph <steph1111>
2024-01-14 17:17:22 -0800

    Added FeatStateWidget class FeatStateWidget provides a graphical widget which displays the state of various features on the robot This allows the pilot to view what features are currently engaged. Currently the widget displays Bambi mode, wether the claw is open or closed, and if the CoM is shifted or normal

--- src/seahawk/seahawk_deck/dash.py
+++ src/seahawk/seahawk_deck/dash.py
@@ -1,6 +1,8 @@
+# Run 'unset GTK_PATH' in new terminals if installed vs code with snap
+
 from PyQt5 import QtWidgets as qtw
-# from PyQt5 import QtGui as qtg
-# from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
 import sys
 
 # Size constants
@@ -11,13 +13,16 @@ MAX_HEIGHT  = 600  # Temp for debugging
 
 # Color constants
 COLOR_CONSTS = {
-    "MAIN_WIN_BKG"        : "#0f0f0f",
-    "INACTIVE_TAB_BKG"    : "#141414",
-    "INACTIVE_TAB_BD"     : "#2d2938",
-    "ACTIVE_TAB_BKG"      : "#181818",
-    "ACCENT_LIGHT"        : "#9171f8",
-    "PRIMARY_TEXT"        : "#fff0f5",
-    "SECONDARY_TEXT"      : "#8f8c95",
+    "MAIN_WIN_BKG"      : "#0c0c0f",
+    "INACTIVE_TAB_BKG"  : "#141416",
+    "INACTIVE_TAB_BD"   : "#2d2938",
+    "ACTIVE_TAB_BKG"    : "#111113",
+    "SURFACE_DRK"       : "#18181b",
+    "SURFACE_BRIGHT"    : "#34343c",
+    "ACCENT_LIGHT"      : "#9172f8",
+    "ACCENT_DRK"        : "#5c4ba3",
+    "PRIMARY_TEXT"      : "#fff0f5",
+    "SECONDARY_TEXT"    : "#8f8c95",
 }
 
 
@@ -38,12 +43,12 @@ class MainWindow(qtw.QMainWindow):
         self.setGeometry(0, 0, MAX_WIDTH, MAX_HEIGHT)
         
         # Create tabs
-        self.__tab_widget = TabWidget(self, ["Home", "Debug", "Cameras", "Control Mapping"], "dash_styling/tab_widget.txt")
-        self.setCentralWidget(self.__tab_widget)
+        tab_widget = TabWidget(self, ["Home", "Debug", "Cameras", "Control Mapping"], "dash_styling/tab_widget.txt")
+        self.setCentralWidget(tab_widget)
 
         # Display window
-        self.show()
-        # self.showMaximized() # Uncomment in final
+        # self.show()
+        self.showMaximized() # Uncomment in final
 
 
 class TabWidget(qtw.QWidget):
@@ -55,42 +60,112 @@ class TabWidget(qtw.QWidget):
     show a different page by clicking on its tab
     """
 
-    def __init__(self, parent: MainWindow, tab_list: list[str], style_sheet_file: str):
+    def __init__(self, parent: MainWindow, tab_names: list[str], style_sheet_file: str):
         """
         Initialize tabs
         Args:
             parent: Window where to place tabs
-            tab_list: List of tab names
+            tab_names: List of tab names
             style_sheet_file: Style sheet text file formatted as a CSS f-string
         """
         super().__init__(parent)
         
         # Define layout of tabs
-        self.__layout = qtw.QVBoxLayout(self)
+        layout = qtw.QVBoxLayout(self)
+        self.setLayout(layout)
 
         # Initialize tabs
-        self.__tabs = qtw.QTabWidget()
+        tabs = qtw.QTabWidget()
 
         # Create a dict in which the key is the provided name of the tab, and the value is a qtw.QWidget() object
-        self.__tab_dict = {name: qtw.QWidget() for name in tab_list}
+        tab_dict = {name: qtw.QWidget() for name in tab_names}
 
         # Add tabs
-        for name, tab in self.__tab_dict.items():
-            self.__tabs.addTab(tab, name)
+        for name, tab in tab_dict.items():
+            tabs.addTab(tab, name)
         
         # Apply css styling
         with open(style_sheet_file) as style_sheet:
             self.setStyleSheet(style_sheet.read().format(**COLOR_CONSTS))
         
         # Add tabs to widget
-        self.__layout.addWidget(self.__tabs)
-        self.setLayout(self.__layout)
+        layout.addWidget(tabs)
+        
+        # Display feature state node
+        feat_state_widget = FeatStateWidget(tab_dict["Home"], ["Bambi Mode", "Claw", "CoM Shift"], "dash_styling/feat_state_widget.txt")
+        feat_state_widget.resize(180, 150) # FIXME: This should probably not be a fixed value
 
         # What to do when a tab is clicked
         # self.__tabs.currentChanged.connect(self.__on_click)
 
     # @qtc.pyqtSlot()
     # def __on_click(self):
+
+class FeatStateWidget(qtw.QWidget):
+    """
+    Creates a 'FeatStateWidget' which inherits from the 'qtw.QWidget' class. A 'FeatStateWidget' provides 
+    a visual representation of if a feature is engaged or not
+    """
+
+    def __init__(self, parent: qtw.QWidget, feature_names: list[str], style_sheet_file: str):
+        """
+        Initialize feature state widget
+        Args:
+            parent: Widget to overlay 'FeatStateWidget' on
+            feature_names: List of feature names
+            style_sheet_file: Style sheet text file formatted as a CSS f-string
+        """
+        super().__init__(parent)
+
+        # Import state images
+        self.__on_img   = qtg.QPixmap("dash_styling/on_img.svg")
+        self.__off_img  = qtg.QPixmap("dash_styling/off_img.svg")
+        
+        # Track if feature is engaged
+        self.__feat_engaged = {name: False for name in feature_names}
+
+        # Create a dictionary of label objects
+        self.__label_dict = {name: {"feat": qtw.QLabel(), "state": qtw.QLabel()} for name in feature_names}
+
+        # Define layout of frame on parent
+        layout_outer = qtw.QGridLayout(self)
+        self.setLayout(layout_outer)
+
+        # Create frame widget
+        frame = qtw.QFrame()
+        layout_outer.addWidget(frame)
+
+        # Set layout of labels on frame to grid
+        layout_inner = qtw.QGridLayout(frame)
+        frame.setLayout(layout_inner)
+
+        # Set text of each label and add to grid
+        for i, (name, labels) in enumerate(self.__label_dict.items()):
+            labels["feat"].setText(name)
+            labels["state"].setPixmap(self.__off_img)
+            # Grid layout:
+            # (0, 0)    (0, 1)
+            # (1, 0)    (1, 1)
+            layout_inner.addWidget(labels["feat"], i, 0)
+            layout_inner.addWidget(labels["state"], i, 1)
+
+        # Apply css styling
+        with open(style_sheet_file) as style_sheet:
+            self.setStyleSheet(style_sheet.read().format(**COLOR_CONSTS))
+
+    def update_state(self, feature: str):
+        """
+        Update graphical representation of the feature state if the state has changed
+
+        Args:
+            The name of the feature to update
+        """
+        if self.__feat_engaged[feature]:
+            self.__label_dict[feature]["state"].setPixmap(self.__off_img)
+            self.__feat_engaged[feature] = False
+        else:
+            self.__label_dict[feature]["state"].setPixmap(self.__on_img)
+            self.__feat_engaged[feature] = True
 
 
 def main():
