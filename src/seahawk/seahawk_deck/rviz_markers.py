28bb11a0cd48fb9d8ca76f8cfec40c014b1c85ef
steph1111 <stephanielh1111@gmail.com>
2024-01-27 03:04:45 -0800

    Moved constants to in the init

--- src/seahawk/seahawk_deck/rviz_markers.py
+++ src/seahawk/seahawk_deck/rviz_markers.py
@@ -1,5 +1,5 @@
-import sys 
-import math 
+from sys import argv
+from math import pi, cos, sin
 from enum import Enum
 
 import rclpy
@@ -12,40 +12,44 @@ class MarkerMaker(Node):
     """
     Node to visualize the motor controls of the robot using RViz markers
     """
-    # Position constants for top motors (0-3)
-    X_TOP = 0.19
-    Y_TOP = 0.12
-    Z_TOP = 0.047
-    # Position constants for top motors (4-7)
-    X_BOT = 0.105
-    Y_BOT = 0.15
-    Z_BOT = -0.038
-
-    # Motor positions
-    # Position: (x, y, z)       , Rotation: (Roll, Pitch, Yaw)
-    MOTORS = [
-        ((-X_TOP,  Y_TOP, Z_TOP), (0,   math.pi / 2,  0)),      # 0 (Top)
-        ((-X_TOP, -Y_TOP, Z_TOP), (0,   math.pi / 2,  0)),      # 1 (Top)
-        (( X_TOP,  Y_TOP, Z_TOP), (0,   math.pi / 2,  0)),      # 2 (Top)
-        (( X_TOP, -Y_TOP, Z_TOP), (0,   math.pi / 2,  0)),      # 3 (Top)
-        (( X_BOT,  Y_BOT, Z_BOT), (0,   0,  3 * math.pi / 4)),  # 4 (Bottom)
-        (( X_BOT, -Y_BOT, Z_BOT), (0,   0,  5 * math.pi / 4)),  # 5 (Bottom)    
-        ((-X_BOT,  Y_BOT, Z_BOT), (0,   0,  math.pi     / 4)),  # 6 (Bottom)
-        ((-X_BOT, -Y_BOT, Z_BOT), (0,   0,  7 * math.pi / 4)),  # 7 (Bottom)
-    ]
 
     def __init__(self):
         """
         Initialize 'marker_maker' node
         """
+        # Set up node and pub/sub
         super().__init__("marker_maker")
         self.marker_pub = self.create_publisher(MarkerArray, "motor_debug", 10)
         self.subscription = self.create_subscription(Float32MultiArray, "motor_values", self.callback, 10)
 
-        class ObjectTypes(Enum):
-            ARROW, CUBE, TEXT_VIEW_FACING, = 0, 1, 9
+        # Position constants for top motors (0-3)
+        X_TOP = 0.19
+        Y_TOP = 0.12
+        Z_TOP = 0.047
+        # Position constants for top motors (4-7)
+        X_BOT = 0.105
+        Y_BOT = 0.15
+        Z_BOT = -0.038
+        # Angle constants
+        ANGLE_TOP = pi / 2
 
+        # Motor position constants
+        # Position: (x, y, z),         Rotation: (Roll, Pitch, Yaw)
+        MOTORS = [
+            ((-X_TOP,  Y_TOP, Z_TOP), (0, ANGLE_TOP,  0)),            # 0 (Top)
+            ((-X_TOP, -Y_TOP, Z_TOP), (0, ANGLE_TOP,  0)),            # 1 (Top)
+            (( X_TOP,  Y_TOP, Z_TOP), (0, ANGLE_TOP,  0)),            # 2 (Top)
+            (( X_TOP, -Y_TOP, Z_TOP), (0, ANGLE_TOP,  0)),            # 3 (Top)
+            (( X_BOT,  Y_BOT, Z_BOT), (0, 0,          3 * pi / 4)),   # 4 (Bottom)
+            (( X_BOT, -Y_BOT, Z_BOT), (0, 0,          5 * pi / 4)),   # 5 (Bottom)    
+            ((-X_BOT,  Y_BOT, Z_BOT), (0, 0,          pi     / 4)),   # 6 (Bottom)
+            ((-X_BOT, -Y_BOT, Z_BOT), (0, 0,          7 * pi / 4)),   # 7 (Bottom)
+        ]
         NUM_MOTORS = 8
+
+        # Enum for label object types
+        class ObjectTypes(Enum):
+            ARROW, TEXT_VIEW_FACING, = 0, 9
 
         self.markers = MarkerArray()
         self.markers.markers = [ Marker() for x in range(NUM_MOTORS * 3) ]   
@@ -64,9 +68,9 @@ class MarkerMaker(Node):
             self.labels[i].scale.x = 0.05
             self.labels[i].scale.y = 0.05
             self.labels[i].scale.z = 0.05
-            self.labels[i].pose.position.x = float(MarkerMaker.MOTORS[i][0][0]) + 0.5 * MarkerMaker.MOTORS[i][0][0] 
-            self.labels[i].pose.position.y = float(MarkerMaker.MOTORS[i][0][1]) + 0.5 * MarkerMaker.MOTORS[i][0][1]
-            self.labels[i].pose.position.z = float(MarkerMaker.MOTORS[i][0][2])
+            self.labels[i].pose.position.x = float(MOTORS[i][0][0]) + 0.5 * MOTORS[i][0][0] 
+            self.labels[i].pose.position.y = float(MOTORS[i][0][1]) + 0.5 * MOTORS[i][0][1]
+            self.labels[i].pose.position.z = float(MOTORS[i][0][2])
             self.labels[i].color.r = 1.0
             self.labels[i].color.g = 1.0
             self.labels[i].color.b = 1.0
@@ -78,10 +82,10 @@ class MarkerMaker(Node):
             self.arrows[i].scale.x = 0.0
             self.arrows[i].scale.y = 0.05
             self.arrows[i].scale.z = 0.05
-            self.arrows[i].pose.position.x = float(MarkerMaker.MOTORS[i][0][0])
-            self.arrows[i].pose.position.y = float(MarkerMaker.MOTORS[i][0][1])
-            self.arrows[i].pose.position.z = float(MarkerMaker.MOTORS[i][0][2])
-            self.arrows[i].pose.orientation = self.quaternion_from_euler(*MarkerMaker.MOTORS[i][1])
+            self.arrows[i].pose.position.x = float(MOTORS[i][0][0])
+            self.arrows[i].pose.position.y = float(MOTORS[i][0][1])
+            self.arrows[i].pose.position.z = float(MOTORS[i][0][2])
+            self.arrows[i].pose.orientation = self.quaternion_from_euler(*MOTORS[i][1])
             self.arrows[i].color.r = 1.0
             self.arrows[i].color.g = 0.0
             self.arrows[i].color.b = 0.0
@@ -95,6 +99,7 @@ class MarkerMaker(Node):
         for i, motor in enumerate(motor_msg.data):
             self.arrows[i].scale.x = -motor / 16 # Scale arrows down to a reasonable size
             self.labels[i].text = f"({i}){round(motor, 2)}N"
+            # Apply gradient to thrust arrows
             self.arrows[i].color.r = abs(motor) / MAX_FWD_THRUST
             self.arrows[i].color.g = 0.2
             self.arrows[i].color.b = 0.45
@@ -106,12 +111,12 @@ class MarkerMaker(Node):
         ai /= 2.0
         aj /= 2.0
         ak /= 2.0
-        ci = math.cos(ai)
-        si = math.sin(ai)
-        cj = math.cos(aj)
-        sj = math.sin(aj)
-        ck = math.cos(ak)
-        sk = math.sin(ak)
+        ci = cos(ai)
+        si = sin(ai)
+        cj = cos(aj)
+        sj = sin(aj)
+        ck = cos(ak)
+        sk = sin(ak)
         cc = ci * ck
         cs = ci * sk
         sc = si * ck
@@ -133,4 +138,4 @@ def main(args=None):
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(argv)
