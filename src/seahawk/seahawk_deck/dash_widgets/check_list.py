f8a9e44e4962f2ecf85754071c3ddedc5e4a7b35
Massimo Ginella <massimoginella12@gmail.com>
2024-02-22 07:17:41 +0000

    added widget creation code & some button code

--- src/seahawk/seahawk_deck/dash_widgets/check_list.py
+++ src/seahawk/seahawk_deck/dash_widgets/check_list.py
@@ -26,12 +26,64 @@ class CheckList(qtw.QWidget):
         """
         super().__init__(parent)
 
-        # Your code here
+        # Create layout where widget will be mounted on parent
+        outer_layout = qtw.QVBoxLayout(self)
+        self.setLayout(outer_layout)
+
+        # Add a widget to mount to the outer layout
+        frame = qtw.QFrame()
+        outer_layout.addWidget(frame)
+
+        # Add a grid to the widget to add text & features
+        inner_layout = qtw.QVBoxLayout(frame)
+        frame.setLayout(inner_layout)
+
+        # Place holders for the category & subcategory strs
+        category_title = ""
+        sub_category_title = ""
+
+        # Creating labels for text to be displayed on
+        self.category = qtw.QLabel()
+        self.sub_category = qtw.QLabel()
+
+        # Giving string values to the created labels
+        self.category.setText(category_title)
+        self.sub_category.setText(sub_category_title)
+        
+        # Add variable for checkBox
+        self.checkBox = qtw.QCheckBox()
+
+
        
         # Uncomment later when we add the CSS
         # with open(style_sheet_file) as style_sheet:
         #     self.setStyleSheet(style_sheet.read().format(**COLOR_CONSTS))
-# test.py
+        # test.py
+        
+    """
+    def parse_txt_file (self, task_list_file: str):
+        
+        Reads through the task list file to gather info on tasks to add to widget
+
+        Args:
+            self: current instance of the CheckList class
+            task_list_file: string representing the name of the file that holds the ROV tasks
+        
+        txt_storage = []
+
+        # i want to make a data structure like this ->
+        # vector<pair<string, vector<pair<string, vector<string>>>>> 
+        # so that i can store the text file data in an organized manner, 
+        # but i cant figure out how to format it in python
+
+        with open(task_list_file, 'r') as file:
+            for line in file:
+                if line == "[CAT]":
+                    next(file)  # move ahead one line
+                    txt_storage
+    """
+
+                
 from os import environ
 import sys
 
