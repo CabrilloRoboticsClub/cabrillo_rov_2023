18d47a4915f1ef711a9c14296d6ebbf7887053ef
Massimo Ginella <massimoginella12@gmail.com>
2024-02-24 08:27:06 +0000

    added progress bar and checkbox functionality

--- src/seahawk/seahawk_deck/dash_widgets/check_list.py
+++ src/seahawk/seahawk_deck/dash_widgets/check_list.py
@@ -42,40 +42,82 @@ class CheckList(qtw.QWidget):
         frame.setLayout(inner_layout)
 
         # Creating labels for text to be displayed on
-        self.title = qtw.QLabel()
-        self.task_titles = qtw.QLabel()
+        self.title = qtw.QLabel(parent)
+        self.task_titles = qtw.QLabel(parent)
 
         # Giving string values to the created labels
-        self.title.setText("COMPETITION TASKS:")
+        self.title.setText("TASKS:")
 
-        # Add variable for checkBox
-        self.checkBox = qtw.QCheckBox()
+        # Need to add a scroll bar on a QListWidget
+        list_widget = qtw.QListWidget()
 
-        # Checks if we're in a sub category
-        in_sub_cat = False
+        # Create a scrollbar
+        self.scroll_bar = qtw.QScrollBar()
+
+        # Add the scrollbar to the list widget
+        list_widget.setVerticalScrollBar(self.scroll_bar)
+
+        # Creating the progress bar
+        self.progress_bar = qtw.QProgressBar(parent)
+
+        # Setting dimensions of the progress bar (change this l8r)
+        # (x, y, l, w?)
+        self.progress_bar.setGeometry(500, 40, 400, 25)
 
         # Has the task as a key, and a its # of points as value
         self.task_dict = {}
 
         # Variable to store points of each task
-        points = 0
+        self.points = 0
+
+        # Variable for the total points achievable for comp
+        self.total_points = 0
+
+        # Variable for keeping track of current points earned
+        self.current_points = 0
+
+        # Variable for keeping track of progress bar percentage
+        self.prog_par_percent = 0
 
         # Used later to grab point value out of string task
         point_search = r"(\d+)pts"
 
-        with open('tasks.json', 'r') as file:
+        # Open json file and store it as a dictionary of dictionaries of strings and lists
+        with open(task_list_file, 'r') as file:
             data_list = json.load(file)
 
         for part, tasks_dict in data_list.items():
             for task_title, tasks_list in tasks_dict.items():
-                self.task_titles.setText(task_title)
+                inner_layout.addSpacing(20)  # Space things out by 20 pixels
+                self.task_titles = qtw.QLabel(parent)  # Create new task title for each task_title
+                self.task_titles.setText(task_title)  # Set the task_title text
+                inner_layout.addWidget(self.task_titles)  # Add the task title as a widget to the inner layout
                 for task in tasks_list:
-                    self.checkBox.setText(task)
-                    match = re.search(point_search, task)
-                    points = int(match.group(1))
-                    self.task_dict[task] = points
+                    self.checkBox = qtw.QCheckBox(parent)  # Create a new check box for each task
+                    self.checkBox.setText(task)  # Add the task text
+                    inner_layout.addWidget(self.checkBox)  # Add the checkbox widget onto the inner_layout
+                    match = re.search(point_search, task)  # Parse the task for its point value
+                    self.points = int(match.group(1))  # Add the point value to the points variable
+                    self.total_points += self.points
+                    self.task_dict[task] = self.points  # Store the task and its point value in a dictionary
+                    inner_layout.addSpacing(10)  # Space the check boxes out
+                    self.checkBox.stateChanged.connect(self.check_box_state)  # Keep track of each checkbox state
+                    self.show()  # Idk what this is but it helped format things good
 
-       
+
+
+    def check_box_state (self, state):
+        sender = self.sender()  # Find the checkbox that was pressed
+        
+        if state == qtc.Qt.Checked:  # If the button has been pressed
+            self.current_points += self.task_dict[sender.text()]  # Add to the current score
+        else:  # If the button has been de-selected
+            self.current_points -= self.task_dict[sender.text()]  # Remove from the current score
+
+        self.prog_par_percent = int(100 * (self.current_points/self.total_points))  # Get % of how many points earned
+
+        self.progress_bar.setValue(self.prog_par_percent)  # Update progress bar to this new percentage
+        
         # Uncomment later when we add the CSS
         # with open(style_sheet_file) as style_sheet:
         #     self.setStyleSheet(style_sheet.read().format(**COLOR_CONSTS))
