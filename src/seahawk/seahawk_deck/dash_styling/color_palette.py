b4f800005a004c9b999cc0547b816baa0f4c22a8
steph1111 <stephanielh1111@gmail.com>
2024-03-03 22:22:53 -0800

    First attempt at light mode, colors need to be fixed

--- src/seahawk/seahawk_deck/dash_styling/color_palette.py
+++ src/seahawk/seahawk_deck/dash_styling/color_palette.py
@@ -3,20 +3,53 @@ from os import path
 PATH = path.dirname(__file__)
 
 DARK_MODE = {
-    "MAIN_WIN_BKG"      : "#0c0c0f",
-    "INACTIVE_TAB_BKG"  : "#141416",
-    "INACTIVE_TAB_BD"   : "#2d2938",
-    "ACTIVE_TAB_BKG"    : "#111113",
-    "SURFACE_DRK"       : "#18181b",
-    "SURFACE_BRIGHT"    : "#242429",
-    "ACCENT_LIGHT"      : "#9172f8",
-    "ACCENT_DRK"        : "#5c4ba3",
-    "PRIMARY_TEXT"      : "#fff0f5",
-    "SECONDARY_TEXT"    : "#8f8c95",
-    "WARNING"           : "#EE4B2B",
+    "MAIN_WIN_BKG"      : "#171717",
+    "INACTIVE_TAB_BKG"  : "#1a1a1a",
+    "INACTIVE_TAB_BD"   : "#2f2f2f",
+    "ACTIVE_TAB_BKG"    : "#1a1a1a",
+    "SURFACE_DRK"       : "#212121",
+    "SURFACE_BRIGHT"    : "#2f2f2f",
+    "ACCENT_LIGHT"      : "#ff5900",
+    "PRIMARY_TEXT"      : "#fffbf0",
+    "SECONDARY_TEXT"    : "#ded3b6",
+    "WARNING"           : "#fc3019",
     "DOWN_ARROW"        : f"{PATH}/countdown_down.svg",
     "UP_ARROW"          : f"{PATH}/countdown_up.svg",
+    "MAP_IMG"           : f"{PATH}/product_demo_map_dark.png"
 }
+
+# DARK_MODE = {
+#     "MAIN_WIN_BKG"      : "#1e1f22",
+#     "INACTIVE_TAB_BKG"  : "#232428",
+#     "INACTIVE_TAB_BD"   : "#313338",
+#     "ACTIVE_TAB_BKG"    : "#232428",
+#     "SURFACE_DRK"       : "#2b2d31",
+#     "SURFACE_BRIGHT"    : "#313338",
+#     "ACCENT_LIGHT"      : "#ff5900",
+#     "PRIMARY_TEXT"      : "#fffbf0",
+#     "SECONDARY_TEXT"    : "#ded3b6",
+#     "WARNING"           : "#fc3019",
+#     "DOWN_ARROW"        : f"{PATH}/countdown_down.svg",
+#     "UP_ARROW"          : f"{PATH}/countdown_up.svg",
+# }
+
+
+# DARK_MODE = {
+#     "MAIN_WIN_BKG"      : "#0c0c0f",
+#     "INACTIVE_TAB_BKG"  : "#141416",
+#     "INACTIVE_TAB_BD"   : "#2d2938",
+#     "ACTIVE_TAB_BKG"    : "#111113",
+#     "SURFACE_DRK"       : "#18181b",
+#     "SURFACE_BRIGHT"    : "#242429",
+#     "ACCENT_LIGHT"      : "#ff5900",
+#     "ACCENT_DRK"        : "#5c4ba3",
+#     "PRIMARY_TEXT"      : "#fffbf0",
+#     "SECONDARY_TEXT"    : "#ded3b6",
+#     "WARNING"           : "#fc3019",
+#     "DOWN_ARROW"        : f"{PATH}/countdown_down.svg",
+#     "UP_ARROW"          : f"{PATH}/countdown_up.svg",
+# }
+
 
 LIGHT_MODE = {
     "MAIN_WIN_BKG"      : "#ececee",
@@ -29,7 +62,8 @@ LIGHT_MODE = {
     "ACCENT_DRK"        : "#5c4ba3",
     "PRIMARY_TEXT"      : "#0c0c0f",
     "SECONDARY_TEXT"    : "#242429",
-    "WARNING"           : "#EE4B2B",
+    "WARNING"           : "#fc3019",
     "DOWN_ARROW"        : f"{PATH}/countdown_down.svg",
     "UP_ARROW"          : f"{PATH}/countdown_up.svg",
+    "MAP_IMG"           : f"{PATH}/product_demo_map_dark.png"
 }
