4c7d401749afe295aace1e8d6d31f4611516ede0
steph1111 <stephanielh1111@gmail.com>
2024-03-09 14:28:42 -0800

    Light mode final colors

--- src/seahawk/seahawk_deck/dash_styling/color_palette.py
+++ src/seahawk/seahawk_deck/dash_styling/color_palette.py
@@ -21,14 +21,14 @@ DARK_MODE = {
 }
 
 LIGHT_MODE = {
-    "MAIN_WIN_BKG"      : "#ececee",
-    "TAB_BKG"           : "#fc3019",
-    "INACTIVE_TAB_BD"   : "#2d2938",
-    "SURFACE_PRIMARY"   : "#d0d0d6",
-    "SURFACE_SECONDARY" : "#e3e3e4",
+    "MAIN_WIN_BKG"      : "#e0dcda",
+    "TAB_BKG"           : "#d5d0ce",
+    "INACTIVE_TAB_BD"   : "#c5c0bd",
+    "SURFACE_PRIMARY"   : "#cec9c6",
+    "SURFACE_SECONDARY" : "#c5c0bd",
     "ACCENT"            : "#ff5900",
     "TEXT"              : "#2f2f2f",
-    "TEXT_EMPH"         : "#0c0c0f",
+    "TEXT_EMPH"         : "#171717",
     "WARNING"           : "#fc3019",
     "LINEAR_CURVE"      : f"{PATH}/thrt_crv_linear_lite.svg",
     "CUBIC_CURVE"       : f"{PATH}/thrt_crv_cubed_lite.svg",
