c02a49c046b0a29df6b37ff2cdb032248a46a363
steph1111 <stephanielh1111@gmail.com>
2024-03-22 22:44:52 +0000

    changed frame size to be smaller

--- src/seahawk/launch/rov.launch.py
+++ src/seahawk/launch/rov.launch.py
@@ -8,20 +8,21 @@ from launch_ros.actions import Node
 from launch.substitutions import FindExecutable
 from launch.event_handlers import OnShutdown
 
+# Camera paths
+claw_camera_path = '/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-video-index2'
+top_camera_path = '/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index2'
+front_camera_path = None
+
+# v4l2_devices = subprocess.run("v4l2-ctl --list-devices", shell=True, stdout=subprocess.PIPE, check=True, encoding='utf-8').stdout
+# if m := re.search(r"mmal.*\n\s*(/dev/video\d)", v4l2_devices) is not None:
+#     front_camera_path = m.group(1)
+#     subprocess.run(f'v4l2-ctl --set-ctrl h264_i_frame_period=90 -d {front_camera_path}', shell=True, check=True)
+#     subprocess.run(f'v4l2-ctl --set-ctrl repeat_sequence_header=1 -d {front_camera_path}', shell=True, check=True)
+# else:
+#     print("Front camera not found!")
+#     front_camera_path = None
+
 microros_serial_device = "/dev/ttyS0"
-
-claw_camera_path = '/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-video-index2'
-top_camera_path = '/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-video-index2'
-
-v4l2_devices = subprocess.run("v4l2-ctl --list-devices", shell=True, stdout=subprocess.PIPE, check=True, encoding='utf-8').stdout
-if m := re.search(r"mmal.*\n\s*(/dev/video\d)", v4l2_devices) is not None:
-    front_camera_path = m.group(1)
-    subprocess.run(f'v4l2-ctl --set-ctrl h264_i_frame_period=90 -d {front_camera_path}', shell=True, check=True)
-    subprocess.run(f'v4l2-ctl --set-ctrl repeat_sequence_header=1 -d {front_camera_path}', shell=True, check=True)
-else:
-    print("Front camera not found!")
-    front_camera_path = None
-
 subprocess.run('sudo /usr/local/bin/openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program pico/seahawk.elf verify reset exit"',
                 shell=True,
                 check=True)
@@ -54,7 +55,7 @@ def generate_launch_description():
                 parameters=[{
                     'input_fn': front_camera_path,
                     'fps': 30,
-                    'size': '1280x960',
+                    'size': '640x480',
                     'frame_id': 'front_camera',
                     'qos_overrides./parameter_events.publisher.reliability': 'best_effort',
                     'qos_overrides./parameter_events.publisher.history': 'keep_last',
@@ -79,7 +80,7 @@ def generate_launch_description():
                 parameters=[{
                     'input_fn': str(pathlib.Path(claw_camera_path).resolve()),
                     'fps': 30,
-                    'size': '1280x960',
+                    'size': '640x480',
                     'frame_id': 'claw_camera',
                     'qos_overrides./parameter_events.publisher.reliability': 'best_effort',
                     'qos_overrides./parameter_events.publisher.history': 'keep_last',
@@ -105,7 +106,7 @@ def generate_launch_description():
                 parameters=[{
                     'input_fn': str(pathlib.Path(top_camera_path).resolve()),
                     'fps': 30,
-                    'size': '1280x960',
+                    'size': '640x480',
                     'frame_id': 'top_camera',
                     'qos_overrides./parameter_events.publisher.reliability': 'best_effort',
                     'qos_overrides./parameter_events.publisher.history': 'keep_last',
