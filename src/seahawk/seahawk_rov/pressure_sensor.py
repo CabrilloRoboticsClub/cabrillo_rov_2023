f03ec537cb783f342995e01a39e7d3080dd02452
Massimo Ginella <massimoginella12@gmail.com>
2024-03-22 15:05:41 -0700

    implemented code for pressure sensor

--- src/seahawk/seahawk_rov/pressure_sensor.py
+++ src/seahawk/seahawk_rov/pressure_sensor.py
@@ -1,17 +1,38 @@
 import rclpy
 from rclpy import Node
 
+import ms5837
+from ms5837 import MS5837_02BA(bus=1)
+
 class PressureReading (Node):
     def __init__(self):
         super().__init__('pressure_publisher')  # what?
-        self.publisher_ = self.create_publisher(String, 'pressure_topic', 10)  # create a publisher that publishes messages of type String to pressure_topic
+        self.pressure_publisher_ = self.create_publisher(Pressure, 'pressure_topic', 10)  # create a publisher that publishes messages of type String to pressure_topic
+        self.depth_publisher_ = self.create_publisher(Depth, 'depth_topic', 10)  # create a publisher that publishes messages of type Depth
+
+        sensor = ms5837.MS5837() # might need to specify model
+        sensor.init()  # initialize the sensor
+        water_density = ms5837.DENSITY_FRESHWATER  # set value for the density of water
+        g = 9.81  # gravitational acceleration in m/s^2
+        sensor.setFluidDensity(ms5837.DENISTY_FRESHWATER)  # Set fluid density 997 kg/m^3
+        pascal = ms5837.UNITS_Pa
+        
         timer_period = 0.5  # space messages out by 0.5 seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)  # create a timer for when the messages are published to topic
 
     def timer_callback(self):
-        msg = String()  # create a obj of type String
-        msg.data = "hamburger"  # pressure data will go here later
-        self.publisher_.publish(msg)  # publish msg to pressure_topic
+        pressure_msg = Pressure()  # create a obj of type Pressure
+        sensor.read(ms5837.OSR_256)  # Read the sensor and update the pressure and temperature.
+        pressure_data = sensor.pressure(pascal)
+        pressure_msg.data = pressure_data  # Get pressure data and give it to msg
+        self.pressure_publisher_.publish(pressure_msg)  # publish pressure_msg to pressure_topic
+
+        depth_msg = Depth()  # create object of type Depth
+        depth_data = pressure_data / (water_density * g)  # getting depth using formula d = pg(roh)
+        depth_msg.data = depth_data  # det the depth_msgs data to the calculated depth
+        self.depth_publisher_.publish(depth_msg)  # publish depth_msg to depth_topic
+
+        
 
 
 def main(args=None):
